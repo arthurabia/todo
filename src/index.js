03bbffc9d1cb5e716814e32ef011c2d775d6ec63
root <root@ARTYORDY.localdomain>
2020-06-03 00:30:51 +0200

    Added the time functionnality and object creation

--- src/index.js
+++ src/index.js
@@ -1,13 +1,37 @@
 import {todoFactory} from "./todoFactory.js"
 
-
-var button = document.getElementById("button1")
-var title = document.getElementById("input1").value
-button.addEventListener("click",function(){
-   const newTodo = todoFactory(title)
-   console.log(newTodo)
+const displayCurrentTime = () =>{
+   let currentTime = new Date();
+   let currentTimeFormatted = ((currentTime.getHours() >= 10) ? currentTime.getHours() : '0' + currentTime.getHours()) + ':' + 
+                              ((currentTime.getMinutes() >= 10) ? currentTime.getMinutes() : '0' + currentTime.getMinutes()) + ':' + 
+                              ((currentTime.getSeconds() >= 10) ? currentTime.getSeconds() : '0' + currentTime.getSeconds()) 
+   document.getElementsByClassName('real')[0].textContent = currentTimeFormatted;
 }
-)
+const displayCurrentDay = () => {
+   let currentTime = new Date();
+   document.getElementsByClassName('header-timer')[0].textContent += ' of day ' + currentTime.getDay() + ' in month ' + currentTime.getMonth();
+}
+
+displayCurrentDay();
+
+const toggleForm = () => {
+   const addTaskForm = document.getElementsByClassName('entry')[0];
+   addTaskForm.classList.toggle('hidden');
+}
+
+const openFormButton = document.getElementsByClassName('todo-list')[0];
+openFormButton.addEventListener('click', toggleForm);
+
+const addTaskButton = document.getElementsByClassName("small-button")[0];
+addTaskButton.addEventListener("click", function () {
+   const titleTask = document.getElementsByClassName("small-entry")[0].value;
+   const descriptionTask = document.getElementsByClassName("large-entry")[0].value;
+   const newTask = todoFactory(titleTask, descriptionTask);
+   console.log(newTask);
+   toggleForm();
+})
+
+const currentTime = setInterval(displayCurrentTime, 1000);
 
 //Create a list of projects, none being a project
 //For each projects, display its list of todos
--- src/todoFactory.js
+++ src/todoFactory.js
@@ -1,6 +1,7 @@
-const todoFactory = (dateAdded,title,description) => {
+const todoFactory = (title,description) => {
+    const dateAdded = new Date();
     let isDone = false;
-    return {dateAdded,title,description,isDone}
-}
+    return {dateAdded,title,description,isDone};
+};
 
 export {todoFactory}
