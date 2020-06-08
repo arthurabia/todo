6f80c09d8debec90dc80ce41ce03a16920a3802b
root <root@ARTYORDY.localdomain>
2020-06-08 22:53:26 +0200

    Finished the todo functionnality

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import {todoFactory} from "./todoFactory.js"
+
 //Do time and day
 const displayCurrentTime = () =>{
    let currentTime = new Date();
@@ -11,68 +12,83 @@ setInterval(displayCurrentTime, 1000);
 
 const displayCurrentDay = (() => {
    let currentTime = new Date();
-   document.getElementsByClassName('header-timer')[0].textContent += ' of day ' + currentTime.getDay() + ' in month ' + (currentTime.getMonth() + 1);
+   document.getElementsByClassName('header-timer')[0].textContent += ' day ' + currentTime.getDate() + ' in month ' + (currentTime.getMonth() + 1);
 })();
 
-
+//Do form interactivity
 const toggleForm = () => {
       const addTaskForm = document.getElementsByClassName('entry')[0];
       addTaskForm.classList.toggle('hidden');
+      document.getElementsByClassName('small-entry')[0].value = '';
+      document.getElementsByClassName('large-entry')[0].value = '';
 }
 
-//Do form interactivity
 const openFormButton = document.getElementsByClassName('sort')[0];
 openFormButton.addEventListener('click', function (e) {
    e.stopPropagation();
 });
 openFormButton.addEventListener('click', toggleForm);
 
-
 // Save tasks
 let taskList = [];
 
 const displayTasks = () => {
+   let taskTextDescription = document.getElementsByClassName('tododescription')[0];
+   taskTextDescription.textContent = '';
    let taskListInHtml = document.getElementsByClassName('todo');
    let numberOfDeletes = taskListInHtml.length;
    let taskParent = document.getElementsByClassName('todo-list')[0];
    if (numberOfDeletes > 1) {
-      console.log('max is ' + numberOfDeletes)
       for (let i = 1; i < numberOfDeletes; i++) {
-         console.log(i);
          taskParent.removeChild(taskParent.lastElementChild);
-         console.log("1 element removed");
       }
    }
-   let pendingTaskList = taskList.filter(x => x.isDone == false)
-   for (const task of pendingTaskList) {
+
+   let pendingTaskList = taskList.filter(x => x.isDone === false)
+   console.log(pendingTaskList)
+   for (let task of pendingTaskList) {
       let taskInHtml = document.createElement('div');
-      taskInHtml.classList.add('todo');
       let taskTitle = document.createElement('div');
-      taskTitle.classList.add('kanji-four', 'jap');
-      taskTitle.textContent = 'ロレムイプサム';
       let taskDescription = document.createElement('div');
+      taskInHtml.classList.add('todo');     
+      taskTitle.classList.add('kanji-four', 'jap');
+      taskTitle.textContent = 'ロレムイプサム';    
       taskDescription.classList.add('todo-text');
       taskDescription.textContent = task.title;
       taskInHtml.appendChild(taskTitle);
       taskInHtml.appendChild(taskDescription);
       taskInHtml.addEventListener('click',function(){
-         let taskDescription = document.getElementsByClassName('tododescription')[0]
-         taskDescription.textContent = task.description
+         taskTextDescription.setAttribute("order", pendingTaskList.indexOf(task))
+         taskTextDescription.textContent = task.description;
       })
       taskParent.appendChild(taskInHtml);
    }
 }
+let deleteButton = document.getElementsByClassName("checkbox")[0]
+deleteButton.addEventListener("click", function (e) {
+   let pendingTaskList = taskList.filter(x => x.isDone === false)
+   let index = document.getElementsByClassName('tododescription')[0]
+   index = index.getAttribute("order")
+   pendingTaskList[index].isDone = true;
+   displayTasks();
+   e.stopPropagation();
+})
 
 const addTaskButton = document.getElementsByClassName("small-button")[0];
 addTaskButton.addEventListener("click", function () {
-   const titleTask = document.getElementsByClassName("small-entry")[0].value;
-   const descriptionTask = document.getElementsByClassName("large-entry")[0].value;
+   let titleTask = document.getElementsByClassName("small-entry")[0].value;
+   let descriptionTask = document.getElementsByClassName("large-entry")[0].value;
    const newTask = todoFactory(titleTask, descriptionTask);
    taskList.push(newTask);
+   //console.log(taskList)
    toggleForm();
    displayTasks();
 })
 
 
-//Create a list of projects, none being a project
-//For each projects, display its list of todos
+
+
+//Replace bien le bouton Rouge
+//Ajoute la possibilité de changer le isdone to true
+//Changer le see completed et le see pending
+//Préparer le mode danger
